5868aa9c2e89a0e097400c0aea6ca90756d36eac
Avery Bentz <averybentz@bell.net>
2016-01-24 17:03:31 -0500

    uworkinglg login

--- app.js
+++ app.js
@@ -3,37 +3,40 @@
 */
 var express = require('express'),
     bodyParser = require('body-parser'),
-    cookieParser = require('cookie-parser'),
     session = require('express-session'),
 	http = require('http'),
 	mongoose = require('mongoose');
-	//hash = require('./pass').hash;
+    hash = require('./pass').hash;
 
 var app = express();
 
 /*
  Database and Models
 */
-mongoose.connect("mongodb://localhost/test"); //FIX ME
-var UserSchema = new mongoose.Schema({
-	username: String,
-	password: String,
-    //salt: String, //Used in encrypting
-	//hash: String //Also used in encrypting
+
+var db = mongoose.connection;
+db.on('error', console.error);
+db.once('open', function() {
 });
 
+mongoose.connect("mongodb://localhost/test/users"); //FIX ME
+
+var UserSchema = new mongoose.Schema({
+	username: String,
+	password: String
+    //salt: String,
+    //hash: String
+});
 
 var User = mongoose.model('users', UserSchema); //Reads into 'users' collection
 /*
  Middlewares and Configurations
 */
-app.use(bodyParser.json());
-app.use(cookieParser('Authentication Tutorial '));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(session({
-  secret: 'keyboard cat',
+  secret: 'ssshhhhh',
   resave: false,
   saveUninitialized: true,
-  cookie: { secure: true }
 }));
 app.use(express.static('public'));
 app.set('views', __dirname + '/views');
@@ -56,22 +59,19 @@ function authenticate(name, pass, fn) {
     if (!module.parent) console.log('authenticating %s:%s', name, pass);
 
     User.findOne({
-        username: name
+        username: name,
+        password: pass,
     },
 
     function (err, user) { //How does it know what variables are meant?
         if (user) {
             if (err) return fn(new Error('cannot find user'));
-            
-            if (pass != user.password) return fn(null, user);
-            fn(new Error('invalid password'));
-            /*
-            hash(pass, user.salt, function (err, hash) {
-                if (err) return fn(err);
-                if (pass == user.hash) return fn(null, user);
+            //hash(pass, user.salt, function (err, hash) {
+                //if (err) return fn(err);
+                if (pass == user.password) return fn(null, user);
                 fn(new Error('invalid password'));
-            });
-            */
+            //});
+            
         } else {
             return fn(new Error('cannot find user'));
         }
@@ -109,6 +109,7 @@ app.get("/", function (req, res) {
         res.send("Welcome " + req.session.user.username + "<br>" + "<a href='/logout'>logout</a>");
     } else {
         res.send("<a href='/login'> Login</a>" + "<br>" + "<a href='/signup'> Sign Up</a>");
+        console.log(req.session.user);
     }
 });
 
@@ -124,23 +125,27 @@ app.post("/signup", userExist, function (req, res) {
     var pw = req.body.password;
     var un = req.body.username;
 
-    var user = new User({
-        username: un,
-        password: pw,
-        //hash: hash,
-    }).save(function (err, newUser) {
-        if (err) throw err;
-        authenticate(newUser.username, newUser.password, function(err, user){
-            if(user){
-                req.session.regenerate(function(){
-                    req.session.user = user;
-                    req.session.success = 'Authenticated as ' + user.username + ' click to <a href="/logout">logout</a>. ' + ' You may now access <a href="/restricted">/restricted</a>.';
-                    res.redirect('/');
-                });
-            }
+    //hash(pw, function (err, salt, hash) {
+        //if (err) throw err;
+        var user = new User({
+            username: un,
+            //salt: salt,
+            //hash: hash,
+            password: pw,
+        }).save(function (err, newUser) {
+            if (err) throw err;
+            authenticate(newUser.username, newUser.password, function(err, user){
+                if(user){
+                    req.session.regenerate(function(){
+                        req.session.user = user;
+                        req.session.success = 'Authenticated as ' + user.username + ' click to <a href="/logout">logout</a>. ' + ' You may now access <a href="/restricted">/restricted</a>.';
+                        res.redirect('/');
+                    });
+                }
+            });
         });
     });
-});
+//});
 
 app.get("/login", function (req, res) {
     res.render("login");
